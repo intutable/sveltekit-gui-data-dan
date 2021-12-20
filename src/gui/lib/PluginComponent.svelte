0fbcbf53ccdd8495dba18441c77fd288db2417aa
yakuhzi <miguel@heidegger-ueberlingen.de>
2021-12-20 12:46:09 +0100

    feat: code editor

--- src/gui/lib/PluginComponent.svelte
+++ src/gui/lib/PluginComponent.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+    import CodeEditor from "./editor/CodeEditor.svelte"
+
+    let codeSnippet: string
 </script>
 
-DataDan Plugin Component
+<div class="main-container">
+    <CodeEditor bind:codeSnippet={codeSnippet}/>
+</div>
 
 <style lang="sass">
+  @use "../style/theme"
+
+  .main-container
+    @extend .theme-plain
+    display: flex
+    flex-direction: column
 </style>
---
+++ src/gui/lib/editor/CodeEditor.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import * as Monaco from "monaco-editor"
+    import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker"
+    import TsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker"
+    import { onMount } from "svelte"
+
+    export let codeSnippet: string
+
+    let divElement: HTMLDivElement | undefined = undefined
+    let editor: Monaco.editor.IStandaloneCodeEditor
+
+    $: {
+        codeSnippet = editor?.getValue() ?? ""
+    }
+
+    onMount(async () => {
+        // @ts-ignore
+        self.MonacoEnvironment = {
+            getWorker: (_: any, label: string) => {
+                return label === "javascript" ? new TsWorker() : new EditorWorker()
+            }
+        }
+
+        editor = Monaco.editor.create(divElement, {
+            value: ["function x() {", "\tconsole.log(\"Hello world!\");", "}"].join("\n"),
+            language: "javascript",
+            theme: "vs-dark",
+            minimap: {
+                enabled: false
+            },
+            scrollbar: {
+                horizontalScrollbarSize: 5,
+                verticalScrollbarSize: 5
+            },
+            scrollBeyondLastLine: false,
+        })
+
+        return () => { editor.dispose() }
+    })
+</script>
+
+<div class="editor">
+    <div class="monaco-editor" bind:this={divElement}></div>
+</div>
+
+<style lang="sass">
+  .editor
+    border: 2px solid #1e1e1e
+    border-radius: 6px
+    padding: 0.5rem
+    background: #1e1e1e
+    height: 14rem
+
+  .monaco-editor
+    height: 100%
+    overflow: hidden
+</style>
