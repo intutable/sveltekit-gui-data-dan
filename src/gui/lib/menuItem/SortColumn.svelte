34881b4a5f21d1089d538f2e4b9d7444b2623d80
yakuhzi <miguel@heidegger-ueberlingen.de>
2022-01-17 08:56:00 +0100

    feat: use table context

--- src/gui/lib/menuItem/SortColumn.svelte
+++ src/gui/lib/menuItem/SortColumn.svelte
@@ -3,23 +3,22 @@
     import { executeCodeSnippet } from "../fetch"
     import { Output, OutputType } from "../component/output/types"
     import { RequestContext, RequestError, StoreContext } from "../types"
+    import type { TableContext } from "./types"
 
     const requestContext = getContext<RequestContext>("request")
     const storeContext = getContext<StoreContext>("store")
-    const TABLE_NAME = "p1_newTableName"
+
+    export let tableContext: TableContext
 
     async function sortColumn() {
+        console.log(`Sort column '${tableContext.columnName}' of table '${tableContext.tableName}'`)
+
         try {
-            const codeSnippet = "TABLE = TABLE.sort_values({ by: 'newColumnName' })"
+            const codeSnippet = `TABLE = TABLE.sort_values({ by: '${tableContext.columnName}' })`
             const response = await executeCodeSnippet(codeSnippet, requestContext)
-
-            const message = response.message.charAt(0).toUpperCase() + response.message.slice(1)
-            const output = new Output(OutputType.Info, message)
-            console.log(output)
-
-            await storeContext.updateRows(TABLE_NAME, response.data)
+            await storeContext.updateRows(`p1_${tableContext.tableName}`, response.data)
         } catch (error: RequestError) {
-            console.log(error)
+            console.log(`Sort column request failed with error: '${error.body.error}'`)
         }
     }
 </script>
