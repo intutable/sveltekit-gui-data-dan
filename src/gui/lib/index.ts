c1a299d1717b139c176c82f6d8f35c17acbead1e
yakuhzi <miguel@heidegger-ueberlingen.de>
2022-03-06 17:57:18 +0100

    feat: save tables on commit

--- src/gui/lib/index.ts
+++ src/gui/lib/index.ts
@@ -1,36 +1,40 @@
 import EditorComponent from "./editor/EditorComponent.svelte"
 import { executeCodeSnippet } from "./editor/fetch"
-import { loadTable, refreshTableData, removeTable } from "./fetch"
+import { commitChanges, loadTable, refreshTableData, removeTable } from "./fetch"
 import { refreshHistory } from "./history/fetch"
 import HistoryComponent from "./history/HistoryComponent.svelte"
 import type { RequestContext, StoreContext, TableListener } from "./types"
 
-async function onInit(requestContext: RequestContext, storeContext: StoreContext): Promise<void> {
+let requestContext: RequestContext
+let storeContext: StoreContext
+
+async function onInit(_requestContext: RequestContext, _storeContext: StoreContext): Promise<void> {
+    requestContext = _requestContext
+    storeContext = _storeContext
     await refreshTableData(requestContext, storeContext)
 }
 
 const tableListener: TableListener = {
-    onRefresh: async (requestContext, storeContext) => {
+    onRefresh: async () => {
         await refreshTableData(requestContext, storeContext)
         await refreshHistory(requestContext)
     },
-    onLoad: async (tableName, requestContext) => {
+    onLoad: async (tableName) => {
         await loadTable(`p1_${tableName}`, requestContext)
         await refreshHistory(requestContext)
     },
-    onDelete: async (tableName, requestContext) => {
+    onDelete: async (tableName) => {
         await removeTable(`p1_${tableName}`, requestContext)
         await refreshHistory(requestContext)
     },
-    onUpdate: async (
-        tableName,
-        rowIndex,
-        columnIndex,
-        newValue,
-        requestContext
-    ) => {
+    onUpdate: async (tableName, rowIndex, columnIndex, newValue) => {
         const code = `p1_${tableName}.values[${rowIndex}][${columnIndex + 1}] = "${newValue}";`
         await executeCodeSnippet(code, requestContext)
+    },
+    onCommit: async () => {
+        await commitChanges(requestContext)
+        await refreshTableData(requestContext, storeContext)
+        await refreshHistory(requestContext)
     }
 }
 
